329df24875ffa0e867524d9733b4f6774ab35dc3
Nikhil Math <erulemath@gmail.com>
2020-12-10 20:36:51 -0700

    did some work

---
+++ index.js
@@ -0,0 +1,52 @@
+var shoot = [
+	'rock',
+	'paper',
+	'scissors'
+];
+
+const random = Math.floor(Math.random() * shoot.length);
+console.log(random, shoot[random]);
+
+
+document.getElementById('userInput').innerHTML = userInput
+
+// handleClick = (choice) => {
+// 	if (choice === '') {
+// 		leftSide += num;
+// 		document.getElementById('leftside').innerHTML = leftSide;
+// 	} else {
+// 		rightSide += num;
+// 		document.getElementById('rightside').innerHTML = rightSide;
+// 	}
+// };
+
+
+
+
+
+
+// if (userChoice === 'paper') {
+// 	if (computerChoice === 'rock') {
+// 		return 'paper wins';
+// 	} else {
+// 		if (computerChoice === 'scissors') {
+// 			return 'scissors wins';
+// 		}
+// 	}
+// 	if (userChoice === 'scissors') {
+// 		if (computerChoice === 'rock') {
+// 			return 'rock wins';
+// 		} else {
+// 			if (computerChoice === 'paper') {
+// 				return 'scissors wins';
+// 			}
+// 		}
+// 	}
+// }
+
+
+
+
+// psudo code
+// Computers choice
+// who wins the laws. 
